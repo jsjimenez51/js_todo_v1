dff6e00d7fee3800dcc0fd61ebc2d162bff6fec2
Joshua S. Jimenez <Jsjimenez51@gmail.com>
2019-06-07 14:31:57 -0700

    ADD: JS functionality initial load

---
+++ public/app.js
@@ -0,0 +1,8 @@
+const addForm = document.querySelector('.add');
+
+addForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const newToDo = addForm.add.value.trim(); //removes white spaces of <string>
+    
+});
